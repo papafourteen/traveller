6956426e87e77eebffcf8b280064b49b1542473a
papafourteen <andris.for@hotmail.com>
2023-12-04 19:02:21 +0100

    about change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,11 +11,12 @@ export const About = () => {
       <div className="about-section">
         <img src={about1} alt="about 1" />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis eos consequatur eligendi, tempore impedit ad consequuntur voluptatum deleniti earum aut dolorum dignissimos id ratione saepe perferendis voluptates accusantium placeat nisi?</p>
+        </div>
         <div className="about-section">
           
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, vero totam a suscipit rem sunt maiores quod, cumque recusandae et quae praesentium assumenda ut illo quos! Saepe voluptatem non facilis.</p>
         <img src={about2} alt="about2" />
-        </div>
+        
       </div>
 
     </div>
